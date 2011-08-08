e98c3a7cc67c41924656c608fe16c519e6498a17
Nicos Karalis <dymytrhy@gmail.com>
2011-08-08 00:12:34 -0300

    removed everithing for a fresh start

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RPGmaster::Application.config.secret_token = '5602b5cb699d1e23e8d8e6656c5333a72b67df782c5f26e3fd94bdfc773641b504dd50ab22b24c37d71b259bb2a6b88688da5be8d79027b4d50575b8edf2435d'
