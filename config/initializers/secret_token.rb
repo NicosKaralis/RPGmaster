da62b656f34404464c7773b77f4ce2412b0c4eb1
Nicos Karalis <dymytrhy@gmail.com>
2011-08-08 00:17:38 -0300

    Re-added rails squeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RPGmaster::Application.config.secret_token = 'ea4add6e4bc77652f5a376c066d706e1db469f059d399897d701ec51a76a6a647bbaa984f50cf4572009bafb1559dd4ff3b8973c920e4d2a1892b4b1cba7ce63'
